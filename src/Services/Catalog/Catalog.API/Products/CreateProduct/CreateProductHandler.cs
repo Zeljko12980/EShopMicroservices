adfb8e4664b5c7bcf951c7a4e0293c66ce5a3f54
Zeljko <ikanoviczeljko095@gmail.com>
2024-12-05 13:59:54 +0100

    Add CQRS, Mapster, Carter, MediatR in my project

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,35 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string name, List<string> category, string description, string imageFile, decimal price)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid id);
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+
+            //Business logic to create a product
+
+            //create Product entity from command object
+            //save to database
+            //return createproductresult result
+
+            var product = new Product
+            {
+                Name = command.name,
+                Category = command.category,
+                Description = command.description,
+                ImageFile = command.imageFile,
+                Price = command.price
+            };
+
+
+
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
