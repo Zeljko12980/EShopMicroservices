4ae298b09c06080c404e1d4f9598c80187bd5557
Zeljko <ikanoviczeljko095@gmail.com>
2025-01-11 15:00:39 +0100

    dodat  dependency injection izmedju layera ordering microservica

---
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+
+            return services;
+        }
+    }
+}
--- src/Services/Ordering/OrderingAPI/Program.cs
+++ src/Services/Ordering/OrderingAPI/Program.cs
@@ -1,7 +1,14 @@
+using Ordering.Application;
+using Ordering.Infrastructure;
+using OrderingAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //add services to the container
-
+builder.Services.
+    AddAplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
 var app = builder.Build();
 
 //configure the http request pipeline
